bebc5e0114156d10c4b7309e8356ad96a756b802
maerd-zinbieL <db1345569965@gmail.com>
2024-10-16 20:14:49 +0800

    truth table

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,13 @@ module Main (main) where
 import LangProp(Identifier(..), LangProp(..))
 import Naive
 import Parser
-import TruthTable
+import TruthTable ( table, TruthTable )
 import qualified CNF as C 
 import qualified LangPropCore as L
 
 -- test = C.transform . L.transform . parseProp'
-test = table . L.transform . parseProp'
+test :: String -> TruthTable
+test = table . parseProp'
 
 main :: IO ()
 main = do
--- src/TruthTable.hs
+++ src/TruthTable.hs
@@ -1,53 +1,89 @@
-module TruthTable (table) where
-import Parser ( parseProp )
-import LangPropCore (Identifier (..), LangPropCore (..), transform)
-import Data.List (nub)
+{-# LANGUAGE InstanceSigs #-}
+
+module TruthTable (TruthTable, table) where
+
 import Control.Exception.Base (assert)
+import qualified Data.Bifunctor as B
+import Data.List (intercalate, nub)
+import qualified LangProp as L'
+import qualified LangPropCore as L
 
-type Env = [Identifier]
+type Env = [L.Identifier]
 
-type Vars = [Identifier]
+type Vars = [L.Identifier]
 
-data TruthTable = TruthTable LangPropCore [Identifier] [([Bool], Bool)] 
+data TruthTable = TruthTable L'.LangProp [L.Identifier] [([Bool], Bool)]
 
 instance Show TruthTable where
-  show (TruthTable _ vars rows) = let width = maxium $ map length vars
-                                  where untrim s width = assert (length s <= count) 
-							(s ++ replicate (width - length s) ' ')
+  show :: TruthTable -> String
+  show (TruthTable p vars rows) =
+    let width = roundM 4 $ maximum $ map (length . show) vars
+        pStr = show p
+        th = joinWith (inMiddle width) (map show vars) ++ " | " ++ pStr
+        sepRow = "\n" ++ map (const '_') th ++ "\n"
+        rows' = map (B.bimap (map showBool) showBool) rows
+        rows'' = intercalate sepRow $ map (\(heads, last) -> joinWith (inMiddle width) heads ++ " | " ++ inMiddle (length pStr) last) rows'
+     in th ++ sepRow ++ rows''
+    where
+      inMiddle width s =
+        assert
+          (length s <= width)
+          ( let diff = width - length s
+                fst = diff `div` 2
+                snd = if even diff then fst else fst + 1
+             in (replicate fst ' ' ++ s ++ replicate snd ' ')
+          )
+
+      joinWith f s = unwords $ map f s
+
+      roundM m n = if n `mod` m == 0 then n else (n `div` m + 1) * m
+
+      showBool True = "T"
+      showBool False = "F"
 
 union :: Vars -> Vars -> Vars
 union vars1 vars2 = nub $ vars1 ++ vars2
 
-member :: Identifier -> Env -> Bool
+member :: L.Identifier -> Env -> Bool
 member = elem
 
-freeVars :: LangPropCore -> Vars
-freeVars (Atom ident) = [ident]
-freeVars (And expr1 expr2) = freeVars expr1 `union` freeVars expr2
-freeVars (Or expr1 expr2) = freeVars expr1 `union` freeVars expr2
-freeVars (Not expr) = freeVars expr
+freeVars :: L.LangPropCore -> Vars
+freeVars (L.Atom ident) = [ident]
+freeVars (L.And expr1 expr2) = freeVars expr1 `union` freeVars expr2
+freeVars (L.Or expr1 expr2) = freeVars expr1 `union` freeVars expr2
+freeVars (L.Not expr) = freeVars expr
 
-eval :: Env -> LangPropCore -> Bool
-eval env (Atom ident) = member ident env
-eval env (And expr1 expr2) = eval env expr1 && eval env expr2
-eval env (Or expr1 expr2) = eval env expr1 || eval env expr2
-eval env (Not expr) = not $ eval env expr
+eval :: Env -> L.LangPropCore -> Bool
+eval env (L.Atom ident) = member ident env
+eval env (L.And expr1 expr2) = eval env expr1 && eval env expr2
+eval env (L.Or expr1 expr2) = eval env expr1 || eval env expr2
+eval env (L.Not expr) = not $ eval env expr
 
 worlds :: Vars -> [[Bool]]
 worlds vars = go $ length vars
-    where go :: Int -> [[Bool]]
-          go 0 = [[]]
-          go n = let rest = go $ n - 1
-                 in map (False :) rest ++
-                    map (True :) rest
+  where
+    go :: Int -> [[Bool]]
+    go 0 = [[]]
+    go n =
+      let rest = go $ n - 1
+       in map (False :) rest
+            ++ map (True :) rest
 
-table :: LangPropCore -> TruthTable
-table p = let vars = freeVars p
-              ws = worlds vars 
-	  in go vars ws 
-    where go :: Vars -> [[Bool]]-> TruthTable
-          go vars ws = let rows = map (\w -> let truthVars = filter (\(ident, val) -> val) (zip vars w)
-						 env = map (\(ident, val) -> ident) truthVars
-					     in (w, eval env p))
-				  ws
-                       in TruthTable p vars rows
+table :: L'.LangProp -> TruthTable
+table p =
+  let p' = L.transform p
+      vars = freeVars p'
+      ws = worlds vars
+   in go p' vars ws
+  where
+    go :: L.LangPropCore -> Vars -> [[Bool]] -> TruthTable
+    go p' vars ws =
+      let rows =
+            map
+              ( \w ->
+                  let truthVars = filter snd (zip vars w)
+                      env = map fst truthVars
+                   in (w, eval env p')
+              )
+              ws
+       in TruthTable p vars rows
